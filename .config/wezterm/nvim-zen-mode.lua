893bab9bf0cd5a95755766bc071760ab8f26cd09
basokant <basokanthan@gmail.com>
2024-02-20 00:31:00 -0500

    do not toggle tab bar with zen mode

--- .config/wezterm/nvim-zen-mode.lua
+++ .config/wezterm/nvim-zen-mode.lua
@@ -13,14 +13,11 @@ M.setup = function()
 					window:perform_action(wezterm.action.IncreaseFontSize, pane)
 					number_value = number_value - 1
 				end
-				overrides.enable_tab_bar = false
 			elseif number_value < 0 then
 				window:perform_action(wezterm.action.ResetFontSize, pane)
 				overrides.font_size = nil
-				overrides.enable_tab_bar = true
 			else
 				overrides.font_size = number_value
-				overrides.enable_tab_bar = false
 			end
 		end
 		window:set_config_overrides(overrides)
