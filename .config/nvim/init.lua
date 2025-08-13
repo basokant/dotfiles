a297e7f3206525a6d3cc0961667ee2df8a04920b
basokant <basokanthan@gmail.com>
2025-08-13 17:43:41 -0400

    add smear-cursor

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,6 +16,10 @@ vim.opt.termguicolors = true
 
 vim.g.mapleader = " "
 
+-- Keep cursor center in Down and Up motions
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
+
 local plugins = {
   -- Catppuccin theme
   {
@@ -205,6 +209,22 @@ local plugins = {
         end,
       }):map("<leader>um")
     end,
+  },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      stiffness = 0.8,                      -- 0.6      [0, 1]
+      trailing_stiffness = 0.5,             -- 0.4      [0, 1]
+      stiffness_insert_mode = 0.7,          -- 0.5      [0, 1]
+      trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+      damping = 0.8,                        -- 0.65     [0, 1]
+      damping_insert_mode = 0.8,            -- 0.7      [0, 1]
+      distance_stop_animating = 0.5,        -- 0.1      > 0
+
+      smear_between_buffers = false,
+      smear_insert_mode = false,
+      smear_to_cmd = false,
+    },
   }
 }
 
