3eba8aade4118eca3584498da8a849a0a0dc03a1
basokant <basokanthan@gmail.com>
2025-08-13 17:40:03 -0400

    add render-markdown support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -174,6 +174,37 @@ local plugins = {
         }
       }
     }
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = { sign = true },
+      bullet = { right_pad = 1 },
+      quote = { repeat_linebreak = true },
+      win_options = {
+        showbreak = { default = "", rendered = "  " },
+        breakindent = { default = false, rendered = true },
+        breakindentopt = { default = "", rendered = "" },
+      },
+    },
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      Snacks.toggle({
+        name = "Render Markdown",
+        get = function()
+          return require("render-markdown.state").enabled
+        end,
+        set = function(enabled)
+          local m = require("render-markdown")
+          if enabled then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      }):map("<leader>um")
+    end,
   }
 }
 
@@ -274,11 +305,7 @@ MiniClue.setup({
 MiniFiles = require("mini.files")
 MiniFiles.setup()
 
-vim.keymap.set('n', '<leader>e', function()
-  if MiniFiles.close() == nil then
-    MiniFiles.open()
-  end
-end, { desc = 'Toggle MiniFiles' })
+vim.keymap.set('n', '<leader>e', MiniFiles.open, { desc = 'Open MiniFiles' })
 
 vim.keymap.set("n", "<leader>bd", "<CMD>bd<CR>", { desc = "Close current buffer" })
 
