7730e91d4fd73e2795ad22429a727c3a5720fe4e
basokant <basokanthan@gmail.com>
2025-08-16 16:13:19 -0400

    remove smear-cursor plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -225,22 +225,6 @@ local plugins = {
 			end, { desc = "Toggle Render Markdown" })
 		end,
 	},
-	{
-		"sphamba/smear-cursor.nvim",
-		opts = {
-			stiffness = 0.8, -- 0.6      [0, 1]
-			trailing_stiffness = 0.5, -- 0.4      [0, 1]
-			stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
-			trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
-			damping = 0.8, -- 0.65     [0, 1]
-			damping_insert_mode = 0.8, -- 0.7      [0, 1]
-			distance_stop_animating = 0.5, -- 0.1      > 0
-
-			smear_between_buffers = false,
-			smear_insert_mode = false,
-			smear_to_cmd = false,
-		},
-	},
 }
 
 -- Setup Lazy
