93a81ea13cad760d68d0190170351349d32d6fd4
basokant <basokanthan@gmail.com>
2026-06-20 23:34:27 -0400

    remove deno lsp config fixes (they are built in now)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,8 +2,8 @@ vim.opt.relativenumber = true
 vim.o.confirm = true
 
 if vim.fn.executable("rg") == 1 then -- Use rg for grep
-	vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
-	vim.opt.grepformat = "%f:%l:%c:%m,%f:%l:%m"
+  vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
+  vim.opt.grepformat = "%f:%l:%c:%m,%f:%l:%m"
 end
 
 vim.opt.expandtab = true
@@ -34,201 +34,185 @@ local plugins = { ---@type LazySpec[]
       vim.cmd.colorscheme("electron_highlighter")
     end
   },
-	{ "nvim-mini/mini.basics", opts = {} },
-	{ "nvim-mini/mini.icons", opts = {} },
-	{ "nvim-mini/mini.ai", opts = {} },
-	{ "nvim-mini/mini.pairs", opts = {} },
-	{ "nvim-mini/mini.surround", opts = {} },
-	{ "nvim-mini/mini.bracketed", opts = {} },
-	{ "nvim-mini/mini.statusline", opts = {} },
-	{ "nvim-mini/mini.tabline", opts = {} },
-	{
-		"nvim-mini/mini.files",
-		lazy = false,
-		opts = {},
-		keys = { { "<leader>e", "<cmd>lua MiniFiles.open()<cr>", desc = "File Explorer" } },
-	},
-	{
-		"nvim-mini/mini.diff",
-		lazy = false,
-		opts = { view = { style = "number" } },
-		keys = { { "<leader>go", "<cmd>lua MiniDiff.toggle_overlay()<cr>", desc = "Toggle Git Overlay" } },
-	},
-	{
-		"nvim-mini/mini.clue",
-		config = function(_, _)
-			local gen_clues = require("mini.clue").gen_clues
-			require("mini.clue").setup({
-				triggers = {
-					{ mode = "n", keys = "<Leader>" },
-					{ mode = "n", keys = "g" },
-					{ mode = "n", keys = "'" },
-					{ mode = "n", keys = "`" },
-					{ mode = "x", keys = "'" },
-					{ mode = "x", keys = "`" },
-					{ mode = "n", keys = '"' },
-					{ mode = "i", keys = "<C-r>" },
-					{ mode = "i", keys = "<C-x>" },
-					{ mode = "c", keys = "<C-r>" },
-					{ mode = "n", keys = "<C-w>" },
-					{ mode = "n", keys = "z" },
-					{ mode = "n", keys = "[" },
-					{ mode = "n", keys = "]" },
-				},
-				clues = {
-					gen_clues.builtin_completion(),
-					gen_clues.g(),
-					gen_clues.marks(),
-					gen_clues.registers(),
-					gen_clues.windows(),
-					gen_clues.z(),
-				},
-				window = { delay = 200 },
-			})
-		end,
-	},
-	{
-		"nvim-mini/mini.pick",
-		dependencies = { "nvim-mini/mini.extra", opts = {} },
-		config = function(_, _)
-			require("mini.pick").setup()
-			vim.ui.select = MiniPick.ui_select
-			MiniPick.registry.config = function(local_opts, opts)
-				MiniPick.builtin.files(
-					local_opts or {},
-					vim.tbl_extend("force", opts or {}, { source = { name = "config", cwd = "~/dotfiles" } })
-				)
-			end
-			MiniPick.registry.todos = function(local_opts, opts)
-				MiniPick.builtin.grep(
-					vim.tbl_extend("force", local_opts or {}, { pattern = "TODO|FIXME|NOTE|BUG|HACK" }),
-					opts or {}
-				)
-			end
-		end,
-		keys = {
-			{ "<leader>sf", "<cmd>Pick files<cr>", desc = "Search Files" },
-			{ "<leader>sr", "<cmd>Pick oldfiles<cr>", desc = "Search Recent Files" },
-			{ "<leader>sc", "<cmd>Pick config<cr>", desc = "Search Config" },
-			{ "<leader>st", "<cmd>Pick todos<cr>", desc = "Search Todo Comments" },
-			{ "<leader>/", "<cmd>Pick grep_live<cr>", desc = "Live Grep" },
-			{ "<leader>sb", "<cmd>Pick buffers<cr>", desc = "Search Buffers" },
-			{ "<leader>sh", "<cmd>Pick help<cr>", desc = "Search Help" },
-			{ "<leader>z=", "<cmd>Pick spellsuggest<cr>", desc = "Show spelling suggestions" },
-		},
-	},
-	{
-		"nvim-mini/mini.starter",
-		dependencies = { "nvim-mini/mini.pick" },
-		opts = {
-			header = " ",
-			footer = "",
-			items = {
-				{ name = "Search File  ", action = "Pick files", section = "" },
-				{ name = "Grep Live  ", action = "Pick grep_live", section = "" },
-				{ name = "New File  ", action = "ene | startinsert", section = "" },
-				{ name = "Recent Files  ", action = "Pick oldfiles", section = "" },
-				{ name = "Config  ", action = "Pick config", section = "" },
-				{ name = "Help 󰋖", action = "Pick help", section = "" },
-				{ name = "Lazy 󰒲 ", action = "Lazy", section = "" },
-				{ name = "Quit  ", action = "qa", section = "" },
-			},
-			evaluate_single = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local vue_plugin = {
-				name = "@vue/typescript-plugin",
-				location = "/Users/basokant/Library/pnpm/global/5/node_modules/@vue/language-server",
-				languages = { "vue", "javascript", "typescript" },
-			}
+  { "nvim-mini/mini.basics",     opts = {} },
+  { "nvim-mini/mini.icons",      opts = {} },
+  { "nvim-mini/mini.ai",         opts = {} },
+  { "nvim-mini/mini.pairs",      opts = {} },
+  { "nvim-mini/mini.surround",   opts = {} },
+  { "nvim-mini/mini.bracketed",  opts = {} },
+  { "nvim-mini/mini.statusline", opts = {} },
+  { "nvim-mini/mini.tabline",    opts = {} },
+  {
+    "nvim-mini/mini.files",
+    lazy = false,
+    opts = {},
+    keys = { { "<leader>e", "<cmd>lua MiniFiles.open()<cr>", desc = "File Explorer" } },
+  },
+  {
+    "nvim-mini/mini.diff",
+    lazy = false,
+    opts = { view = { style = "number" } },
+    keys = { { "<leader>go", "<cmd>lua MiniDiff.toggle_overlay()<cr>", desc = "Toggle Git Overlay" } },
+  },
+  {
+    "nvim-mini/mini.clue",
+    config = function(_, _)
+      local gen_clues = require("mini.clue").gen_clues
+      require("mini.clue").setup({
+        triggers = {
+          { mode = "n", keys = "<Leader>" },
+          { mode = "n", keys = "g" },
+          { mode = "n", keys = "'" },
+          { mode = "n", keys = "`" },
+          { mode = "x", keys = "'" },
+          { mode = "x", keys = "`" },
+          { mode = "n", keys = '"' },
+          { mode = "i", keys = "<C-r>" },
+          { mode = "i", keys = "<C-x>" },
+          { mode = "c", keys = "<C-r>" },
+          { mode = "n", keys = "<C-w>" },
+          { mode = "n", keys = "z" },
+          { mode = "n", keys = "[" },
+          { mode = "n", keys = "]" },
+        },
+        clues = {
+          gen_clues.builtin_completion(),
+          gen_clues.g(),
+          gen_clues.marks(),
+          gen_clues.registers(),
+          gen_clues.windows(),
+          gen_clues.z(),
+        },
+        window = { delay = 200 },
+      })
+    end,
+  },
+  {
+    "nvim-mini/mini.pick",
+    dependencies = { "nvim-mini/mini.extra", opts = {} },
+    config = function(_, _)
+      require("mini.pick").setup()
+      vim.ui.select = MiniPick.ui_select
+      MiniPick.registry.config = function(local_opts, opts)
+        MiniPick.builtin.files(
+          local_opts or {},
+          vim.tbl_extend("force", opts or {}, { source = { name = "config", cwd = "~/dotfiles" } })
+        )
+      end
+      MiniPick.registry.todos = function(local_opts, opts)
+        MiniPick.builtin.grep(
+          vim.tbl_extend("force", local_opts or {}, { pattern = "TODO|FIXME|NOTE|BUG|HACK" }),
+          opts or {}
+        )
+      end
+    end,
+    keys = {
+      { "<leader>sf", "<cmd>Pick files<cr>",        desc = "Search Files" },
+      { "<leader>sr", "<cmd>Pick oldfiles<cr>",     desc = "Search Recent Files" },
+      { "<leader>sc", "<cmd>Pick config<cr>",       desc = "Search Config" },
+      { "<leader>st", "<cmd>Pick todos<cr>",        desc = "Search Todo Comments" },
+      { "<leader>/",  "<cmd>Pick grep_live<cr>",    desc = "Live Grep" },
+      { "<leader>sb", "<cmd>Pick buffers<cr>",      desc = "Search Buffers" },
+      { "<leader>sh", "<cmd>Pick help<cr>",         desc = "Search Help" },
+      { "<leader>z=", "<cmd>Pick spellsuggest<cr>", desc = "Show spelling suggestions" },
+    },
+  },
+  {
+    "nvim-mini/mini.starter",
+    dependencies = { "nvim-mini/mini.pick" },
+    opts = {
+      header = " ",
+      footer = "",
+      items = {
+        { name = "Search File  ", action = "Pick files", section = "" },
+        { name = "Grep Live  ", action = "Pick grep_live", section = "" },
+        { name = "New File  ", action = "ene | startinsert", section = "" },
+        { name = "Recent Files  ", action = "Pick oldfiles", section = "" },
+        { name = "Config  ", action = "Pick config", section = "" },
+        { name = "Help 󰋖", action = "Pick help", section = "" },
+        { name = "Lazy 󰒲 ", action = "Lazy", section = "" },
+        { name = "Quit  ", action = "qa", section = "" },
+      },
+      evaluate_single = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local vue_plugin = {
+        name = "@vue/typescript-plugin",
+        location = "/Users/basokant/Library/pnpm/global/5/node_modules/@vue/language-server",
+        languages = { "vue", "javascript", "typescript" },
+      }
 
-			vim.lsp.config("vtsls", { -- Fix vtsls running on deno projects
-				root_dir = function(bufnr, on_dir)
-					local denoRootDir = require("lspconfig.util").root_pattern("deno.json", "deno.json")(bufnr)
-					if denoRootDir then
-						return nil
-					end
-					local root_markers = { "package.json", "yarn.lock", "pnpm-lock.yaml", "bun.lock" }
-					local project_root = vim.fs.root(bufnr, root_markers)
-					if not project_root then
-						return
-					end
-					on_dir(project_root)
-				end,
-				settings = { vtsls = { enableFormatter = false, tsserver = { globalPlugins = { vue_plugin } } } },
-				filetypes = {
-					"javascript",
-					"typescript",
-					"vue",
-					"javascriptreact",
-					"javascript.jsx",
-					"typescriptreact",
-					"typescript.tsx",
-				},
-			})
-			vim.lsp.config( -- Fix denols running on typescript node projects
-				"denols",
-				{ workspace_required = true, root_markers = { "deno.json", "deno.jsonc", "deno.lock" } }
-			)
+      vim.lsp.config("vtsls", {
+        settings = { vtsls = { enableFormatter = false, tsserver = { globalPlugins = { vue_plugin } } } },
+        filetypes = {
+          "javascript",
+          "typescript",
+          "vue",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+      })
 
-			vim.lsp.enable({
-				"lua_ls",
-				"ruff",
-				"pyright",
-				"denols",
-				"vtsls",
-				"astro",
-				"emmet_language_server",
-				"gopls",
-				"golangci_lint_ls",
-				"eslint",
-				"zls",
-				"tinymist",
-				"marksman",
-				"vue_ls",
-				"svelte",
-			})
-		end,
-	},
-	{
-		"stevearc/conform.nvim", -- Formatting
-		opts = { ---@type conform.setupOpts
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				vue = { "prettier" },
-			},
-			format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
-		},
-	},
-	{
-		"saghen/blink.cmp", -- Completion engine
-		dependencies = { "saghen/blink.lib", "rafamadriz/friendly-snippets" },
-	  build = function() require('blink.cmp').build():pwait() end,
-		opts = { ---@module 'blink.cmp' ---@type blink.cmp.Config
-			sources = {
-				default = { "lsp", "path", "snippets" },
-			},
-			signature = { enabled = true },
-		},
-	},
+      vim.lsp.enable({
+        "lua_ls",
+        "ruff",
+        "pyright",
+        "denols",
+        "vtsls",
+        "astro",
+        "emmet_language_server",
+        "gopls",
+        "golangci_lint_ls",
+        "eslint",
+        "zls",
+        "tinymist",
+        "marksman",
+        "vue_ls",
+        "svelte",
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim", -- Formatting
+    opts = { ---@type conform.setupOpts
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        vue = { "prettier" },
+      },
+      format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+    },
+  },
+  {
+    "saghen/blink.cmp", -- Completion engine
+    dependencies = { "saghen/blink.lib", "rafamadriz/friendly-snippets" },
+    build = function() require('blink.cmp').build():pwait() end,
+    opts = { ---@module 'blink.cmp' ---@type blink.cmp.Config
+      sources = {
+        default = { "lsp", "path", "snippets" },
+      },
+      signature = { enabled = true },
+    },
+  },
 }
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then -- Setup Lazy
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath) ---@diagnostic disable-line: undefined-field
 require("lazy").setup(plugins)
